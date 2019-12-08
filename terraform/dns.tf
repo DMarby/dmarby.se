3fb3289d9f5d229be74f7768d65b76f0c987f78d
David Marby <david@dmarby.se>
2019-12-08 22:45:03 +0100

    Add keybase TXT record, update terraform

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -1,20 +1,20 @@
 # dmar.by domain configuration
 resource "digitalocean_domain" "dmar_by" {
   name       = "dmar.by"
-  ip_address = "${digitalocean_floating_ip.dmarby.ip_address}"
+  ip_address = digitalocean_floating_ip.dmarby.ip_address
 }
 
 resource "digitalocean_record" "dmar_by_aaaa" {
-  domain = "${digitalocean_domain.dmar_by.name}"
+  domain = digitalocean_domain.dmar_by.name
   type   = "AAAA"
   name   = "@"
-  value  = "${digitalocean_droplet.dmarby.ipv6_address}"
+  value  = digitalocean_droplet.dmarby.ipv6_address
   ttl    = 60
 }
 
 # CAA records
 resource "digitalocean_record" "dmar_by_caa_issue" {
-  domain = "${digitalocean_domain.dmar_by.name}"
+  domain = digitalocean_domain.dmar_by.name
 
   name  = "@"
   type  = "CAA"
@@ -25,7 +25,7 @@ resource "digitalocean_record" "dmar_by_caa_issue" {
 }
 
 resource "digitalocean_record" "dmar_by_caa_iodef" {
-  domain = "${digitalocean_domain.dmar_by.name}"
+  domain = digitalocean_domain.dmar_by.name
 
   name  = "@"
   type  = "CAA"
@@ -39,21 +39,21 @@ resource "digitalocean_record" "dmar_by_caa_iodef" {
 resource "digitalocean_domain" "dmarby" {
   name = "dmarby.se"
 
-  ip_address = "${digitalocean_floating_ip.dmarby.ip_address}"
+  ip_address = digitalocean_floating_ip.dmarby.ip_address
 }
 
 resource "digitalocean_record" "dmarby_aaaa" {
-  domain = "${digitalocean_domain.dmarby.name}"
+  domain = digitalocean_domain.dmarby.name
   type   = "AAAA"
   name   = "@"
-  value  = "${digitalocean_droplet.dmarby.ipv6_address}"
+  value  = digitalocean_droplet.dmarby.ipv6_address
   ttl    = 60
 }
 
 # CAA records
 
 resource "digitalocean_record" "dmarby_caa_issue" {
-  domain = "${digitalocean_domain.dmarby.name}"
+  domain = digitalocean_domain.dmarby.name
 
   name  = "@"
   type  = "CAA"
@@ -64,7 +64,7 @@ resource "digitalocean_record" "dmarby_caa_issue" {
 }
 
 resource "digitalocean_record" "dmarby_caa_iodef" {
-  domain = "${digitalocean_domain.dmarby.name}"
+  domain = digitalocean_domain.dmarby.name
 
   name  = "@"
   type  = "CAA"
@@ -76,7 +76,7 @@ resource "digitalocean_record" "dmarby_caa_iodef" {
 
 # CNAME records for subdomains
 resource "digitalocean_record" "dmarby_www" {
-  domain = "${digitalocean_domain.dmarby.name}"
+  domain = digitalocean_domain.dmarby.name
   type   = "CNAME"
   name   = "www"
   value  = "dmarby.se."
@@ -84,7 +84,7 @@ resource "digitalocean_record" "dmarby_www" {
 }
 
 resource "digitalocean_record" "dmarby_dump" {
-  domain = "${digitalocean_domain.dmarby.name}"
+  domain = digitalocean_domain.dmarby.name
   type   = "CNAME"
   name   = "dump"
   value  = "dmarby.se."
@@ -93,7 +93,7 @@ resource "digitalocean_record" "dmarby_dump" {
 
 # CNAME records for Google Suite
 resource "digitalocean_record" "dmarby_mail" {
-  domain = "${digitalocean_domain.dmarby.name}"
+  domain = digitalocean_domain.dmarby.name
   type   = "CNAME"
   name   = "mail"
   value  = "ghs.google.com."
@@ -101,7 +101,7 @@ resource "digitalocean_record" "dmarby_mail" {
 }
 
 resource "digitalocean_record" "dmarby_calendar" {
-  domain = "${digitalocean_domain.dmarby.name}"
+  domain = digitalocean_domain.dmarby.name
   type   = "CNAME"
   name   = "calendar"
   value  = "ghs.google.com."
@@ -110,7 +110,7 @@ resource "digitalocean_record" "dmarby_calendar" {
 
 # TXT Records
 resource "digitalocean_record" "dmarby_google" {
-  domain = "${digitalocean_domain.dmarby.name}"
+  domain = digitalocean_domain.dmarby.name
   type   = "TXT"
   name   = "@"
   value  = "google-site-verification=IAo4ufn7P4Y1SDWIKWE8oIh0FuUFRiNYfpgEv7ORqQw"
@@ -118,7 +118,7 @@ resource "digitalocean_record" "dmarby_google" {
 }
 
 resource "digitalocean_record" "dmarby_dkim" {
-  domain = "${digitalocean_domain.dmarby.name}"
+  domain = digitalocean_domain.dmarby.name
   type   = "TXT"
   name   = "google._domainkey"
   value  = "v=DKIM1; k=rsa; p=MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAjWKeB+MTGJm9FkwVinrVnAkEJbRNZhBBEujQuIwnWTVzAgTtGz6WK+YGtQwVpZMScXiS0TOFziq6iTmhjTqw+aXdxduH9/0UuG40SM8BuljoVWupeH9JihrgV6zM1LonHYZBMxNTA9wXKkp6WkyKMKdkLA1w0NUXrcwpaWHYIfdRmUzPcgdphtHck0VZSU380JgJX/N52PwcEX34CLIkhc6ow3hMQZeI9gxTbT1UMKQ4llkk/Lw1UxyF+rIYG4x0qY6woduRjD7sjsAb0NF4QgeUOQKZBBkZg2YnS2qjyrGQ7OXN70HgcUi0UhLwrkXIQOLzBFWJgyNzj/AUtEYTkwIDAQAB"
@@ -126,7 +126,7 @@ resource "digitalocean_record" "dmarby_dkim" {
 }
 
 resource "digitalocean_record" "dmarby_spf" {
-  domain = "${digitalocean_domain.dmarby.name}"
+  domain = digitalocean_domain.dmarby.name
   type   = "TXT"
   name   = "@"
   value  = "v=spf1 include:_spf.google.com ~all"
@@ -135,7 +135,7 @@ resource "digitalocean_record" "dmarby_spf" {
 
 # MX Records
 resource "digitalocean_record" "dmarby_home_mx1" {
-  domain   = "${digitalocean_domain.dmarby.name}"
+  domain   = digitalocean_domain.dmarby.name
   type     = "MX"
   name     = "@"
   value    = "aspmx.l.google.com."
@@ -144,7 +144,7 @@ resource "digitalocean_record" "dmarby_home_mx1" {
 }
 
 resource "digitalocean_record" "dmarby_home_mx2" {
-  domain   = "${digitalocean_domain.dmarby.name}"
+  domain   = digitalocean_domain.dmarby.name
   type     = "MX"
   name     = "@"
   value    = "alt1.aspmx.l.google.com."
@@ -153,7 +153,7 @@ resource "digitalocean_record" "dmarby_home_mx2" {
 }
 
 resource "digitalocean_record" "dmarby_home_mx3" {
-  domain   = "${digitalocean_domain.dmarby.name}"
+  domain   = digitalocean_domain.dmarby.name
   type     = "MX"
   name     = "@"
   value    = "alt2.aspmx.l.google.com."
@@ -162,7 +162,7 @@ resource "digitalocean_record" "dmarby_home_mx3" {
 }
 
 resource "digitalocean_record" "dmarby_home_mx4" {
-  domain   = "${digitalocean_domain.dmarby.name}"
+  domain   = digitalocean_domain.dmarby.name
   type     = "MX"
   name     = "@"
   value    = "alt3.aspmx.l.google.com."
@@ -171,7 +171,7 @@ resource "digitalocean_record" "dmarby_home_mx4" {
 }
 
 resource "digitalocean_record" "dmarby_home_mx5" {
-  domain   = "${digitalocean_domain.dmarby.name}"
+  domain   = digitalocean_domain.dmarby.name
   type     = "MX"
   name     = "@"
   value    = "alt4.aspmx.l.google.com."
@@ -179,9 +179,17 @@ resource "digitalocean_record" "dmarby_home_mx5" {
   priority = 10
 }
 
+resource "digitalocean_record" "dmarby_keybase" {
+  domain = digitalocean_domain.dmarby.name
+  type   = "TXT"
+  name   = "@"
+  value  = "keybase-site-verification=3zsqrHf7kzKcTvDZI2HU78_9rAOV93nGOaW7bHMF4hc"
+  ttl    = 60
+}
+
 # Delegate home.dmarby.se as it's own domain
 resource "digitalocean_record" "dmarby_home_ns1" {
-  domain = "${digitalocean_domain.dmarby.name}"
+  domain = digitalocean_domain.dmarby.name
   type   = "NS"
   name   = "home"
   value  = "ns1.digitalocean.com."
@@ -189,7 +197,7 @@ resource "digitalocean_record" "dmarby_home_ns1" {
 }
 
 resource "digitalocean_record" "dmarby_home_ns2" {
-  domain = "${digitalocean_domain.dmarby.name}"
+  domain = digitalocean_domain.dmarby.name
   type   = "NS"
   name   = "home"
   value  = "ns2.digitalocean.com."
@@ -197,9 +205,10 @@ resource "digitalocean_record" "dmarby_home_ns2" {
 }
 
 resource "digitalocean_record" "dmarby_home_ns3" {
-  domain = "${digitalocean_domain.dmarby.name}"
+  domain = digitalocean_domain.dmarby.name
   type   = "NS"
   name   = "home"
   value  = "ns3.digitalocean.com."
   ttl    = 60
 }
+
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 provider "digitalocean" {
-  token = "${var.do_token}"
+  token = var.do_token
 }
 
 resource "digitalocean_tag" "dmarby" {
@@ -8,7 +8,7 @@ resource "digitalocean_tag" "dmarby" {
 
 resource "digitalocean_ssh_key" "dmarby" {
   name       = "DMarby"
-  public_key = "${file("~/.ssh/id_rsa.pub")}"
+  public_key = file("~/.ssh/id_rsa.pub")
 }
 
 resource "digitalocean_droplet" "dmarby" {
@@ -20,12 +20,13 @@ resource "digitalocean_droplet" "dmarby" {
   monitoring         = false
   private_networking = false
   backups            = false
-  ssh_keys           = ["${digitalocean_ssh_key.dmarby.fingerprint}"]
-  tags               = ["${digitalocean_tag.dmarby.id}"]
-  user_data          = "${file("files/cloud-config.yml")}"
+  ssh_keys           = [digitalocean_ssh_key.dmarby.fingerprint]
+  tags               = [digitalocean_tag.dmarby.id]
+  user_data          = file("files/cloud-config.yml")
 }
 
 resource "digitalocean_floating_ip" "dmarby" {
-  droplet_id = "${digitalocean_droplet.dmarby.id}"
-  region     = "${digitalocean_droplet.dmarby.region}"
+  droplet_id = digitalocean_droplet.dmarby.id
+  region     = digitalocean_droplet.dmarby.region
 }
+
